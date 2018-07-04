2ebc1644189016202e4b74d0aac386e0053b15ab
Anton Dobrohorskyi <zore3x@gmail.com>
2018-07-04 10:12:13 +0300

    добавлен макет отображения списка фотографий

--- app/src/main/java/com/android/zore3x/splashpic/PhotoListActivity.kt
+++ app/src/main/java/com/android/zore3x/splashpic/PhotoListActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class PhotoListActivity : AppCompatActivity() {
 
@@ -14,5 +16,13 @@ class PhotoListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_list)
+
+        App.unsplash.getPhotos()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    for(photo in it) {
+                        photo.description
+                }})
     }
 }
